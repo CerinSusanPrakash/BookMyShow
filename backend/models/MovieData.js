2610baf811ccaf8dcc9b3309366f926dcdea3c7b
CerinSusanPrakash <cerinsusan2002@gmail.com>
2024-11-22 11:48:01 +0530

    Update MovieData.js

--- backend/models/MovieData.js
+++ backend/models/MovieData.js
@@ -1,9 +1,38 @@
-const mongoose=require('mongoose')
-const movieSchema=mongoose.Schema({
-    movieName:String,
-    movieDirector:String,
-    movieDescription:String,
-    movieURL:String
-})
-const MovieData=mongoose.model('movie',movieSchema)
-module.exports=MovieData;
+// const mongoose=require('mongoose')
+// const movieSchema=mongoose.Schema({
+//     movieName:String,
+//     movieDirector:String,
+//     movieDescription:String,
+//     movieURL:String
+// })
+// const MovieData=mongoose.model('movie',movieSchema)
+// module.exports=MovieData;
+
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    director: {
+        type: String,
+        required: true,
+    },
+    releaseYear: {
+        type: Number,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 10,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("movie", movieSchema);
